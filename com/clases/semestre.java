7ec24460a092257b4b933dec53f1f331d2410a3b
andres <andres@example.com>
2014-05-30 18:33:41 -0500

    Agregada clase semestre

---
+++ com/clases/semestre.java
@@ -0,0 +1,12 @@
+public class semestre{
+	String nombre:
+	String apellido;
+	
+	public semestre(String n, String a){
+		nombre = n;
+		apellido = a;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+}
