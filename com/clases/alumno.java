b354337f7efc07a5544e952e88498ed2995466a0
Lalo <manuel@example.com>
2014-05-30 18:14:30 -0500

    Agregado clase Alumno

---
+++ com/clases/alumno.java
@@ -0,0 +1,13 @@
+public class alumno{
+	String nombre:
+	String apellido;
+	
+	public alumno(String n, String a){
+		nombre = n;
+		apellido = a;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+}
