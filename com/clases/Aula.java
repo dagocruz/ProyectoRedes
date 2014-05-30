59a5a6726d4749bd28fe7245d80aceecefcbaf1b
andres <andres@example.com>
2014-05-30 18:42:14 -0500

    Agreagada clase Aula

---
+++ com/clases/Aula.java
@@ -0,0 +1,12 @@
+public class Aula{
+	String nombre:
+	String apellido;
+	
+	public Aula(String n, String a){
+		nombre = n;
+		apellido = a;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+}
